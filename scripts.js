ca17752385635522d9ddc1ab996b4546d6fdcf58
Bss <mx.bss@proton.me>
2025-06-03 18:45:42 +0200

    hover events outside of for loop + clearGrid + allow user to resize

--- scripts.js
+++ scripts.js
@@ -3,6 +3,19 @@ function createGrid(w, h) {
   const colNum = w;
   const rowNum = h;
 
+  // use mouseover to allow delegation
+  container.addEventListener("mouseover", (event) => {
+    if (event.target.classList.contains("col")) {
+      event.target.style.backgroundColor = "black";
+    }
+  });
+
+  container.addEventListener("mouseout", (event) => {
+    if (event.target.classList.contains("col")) {
+      event.target.style.backgroundColor = "gray";
+    }
+  });
+
   for (let i = 0; i < rowNum; i++) {
     const row = document.createElement("div");
     row.classList.add("row");
@@ -10,20 +23,35 @@ function createGrid(w, h) {
     for (let j = 0; j < colNum; j++) {
       const col = document.createElement("div");
       col.classList.add("col");
-
-      col.addEventListener("mouseenter", () => {
-        col.style.backgroundColor = "black";
-      });
-
-      col.addEventListener("mouseleave", () => {
-        col.style.backgroundColor = "gray";
-      });
-
       row.appendChild(col);
     }
 
     container.appendChild(row);
   }
 }
+
+function clearGrid() {
+  const grid = document.querySelector(".container");
+  while (grid.firstChild) {
+    grid.removeChild(grid.lastChild);
+  }
+}
+
+const resizeBtn = document.querySelector("#resize-btn");
+resizeBtn.addEventListener("click", () => {
+  let resize;
+  let valid = false;
+  do {
+    resize = prompt("How many pixels per sides ?\nLess than 100 please..");
+
+    if (resize > 0 && resize <= 100) {
+      valid = true;
+    }
+  } while (!valid);
+
+  console.log(`ok -> ${resize}`);
+  clearGrid();
+  createGrid(resize, resize);
+});
 
 createGrid(16, 16);
